f9a51f7f9080f4bc164f9dbe3c1883522f24763d
sky <cl98914@alibaba-inc.com>
2018-10-24 20:38:31 +0800

    refactor: support action custom

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 Object.assign(exports, require('./lib/builder'));
+exports.Action = require('./lib/action');
 exports.Command = require('./lib/command');
 exports.Download = require('./lib/Download');
 exports.WebpackTool = require('webpack-tool');
