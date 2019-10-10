64abfc48755c4d31b5477ff9508d21a515986e1a
sky <cl98914@alibaba-inc.com>
2019-10-10 19:55:41 +0800

    chore: add typescript method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Object.assign(exports, require('./lib/builder'));
 exports.Action = require('./lib/action');
 exports.Command = require('./lib/command');
 exports.Download = require('./lib/Download');
+exports.utils = require('./lib/utils');
 exports.WebpackTool = require('webpack-tool');
 exports.webpack = exports.WebpackTool.webpack;
 exports.merge = exports.WebpackTool.merge;
