dca70109da1b4f894a18720affe0ea618faf4458
sky <cl98914@alibaba-inc.com>
2020-01-10 16:53:23 +0800

    fix: case sensitive

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 Object.assign(exports, require('./lib/builder'));
 exports.Action = require('./lib/action');
 exports.Command = require('./lib/command');
-exports.Download = require('./lib/Download');
+exports.Download = require('./lib/download');
 exports.WebpackTool = require('webpack-tool');
+exports.utils = require('./lib/utils');
+exports.webpack = exports.WebpackTool.webpack;
+exports.merge = exports.WebpackTool.merge;
